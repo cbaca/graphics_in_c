c0e6a70d341ef6ca1a201989a2ef10f3ac0d2248
Cy Baca <cbaca1@mail.csuchico.edu>
2016-11-12 05:35:02 -0800

    rm old files

--- includes/input.h
+++
@@ -1,5 +0,0 @@
-/** input.h */
-#ifndef INPUT_H_
-#define INPUT_H_
-extern int get_keys();
-#endif
--- toriaezu_matrix/macros.h
+++
@@ -1,5 +0,0 @@
-#ifndef TORIAEZU_MACRO_H_
-#define TORIAEZU_MACRO_H_
-#define MAT_SIZE 16
-#define VEC_SIZE 4
-#endif
